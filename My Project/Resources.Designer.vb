d17a76d4c389dd65443a2aee6cf73dfa0189cf19
LASPUMSS <umssluisalbertosolisp@gmail.com>
2022-08-01 10:24:11 -0600

    UPDATE

--- My Project/Resources.Designer.vb
+++ My Project/Resources.Designer.vb
@@ -59,15 +59,5 @@ Namespace My.Resources
                 resourceCulture = value
             End Set
         End Property
-        
-        '''<summary>
-        '''  Busca un recurso adaptado de tipo System.Drawing.Bitmap.
-        '''</summary>
-        Friend ReadOnly Property RLUV_32x32() As System.Drawing.Bitmap
-            Get
-                Dim obj As Object = ResourceManager.GetObject("RLUV_32x32", resourceCulture)
-                Return CType(obj,System.Drawing.Bitmap)
-            End Get
-        End Property
     End Module
 End Namespace
